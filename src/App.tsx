73eefbcef8d70a8d76f2d3b2fb0efad3e0f19eb5
theblvckdev <nathancodes05@gmail.com>
2024-07-06 14:25:50 +0100

    worked on privacy and terms pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import About from "./pages/about/about";
 import AppPage from "./pages/app/app_page";
 import Events from "./pages/evenets/events";
 import CommunityGuidelines from "./pages/community_guide/community_guidelines";
+import PrivacyPolicy from "./pages/privacy_policy/privacey_policy";
+import TermsCondition from "./pages/terms_condition/terms_condition";
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/app" element={<AppPage />} />
           <Route path="/events" element={<Events />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-and-condition" element={<TermsCondition />} />
           <Route
             path="/community-guidelines"
             element={<CommunityGuidelines />}
---
+++ src/pages/privacy_policy/privacey_policy.tsx
@@ -0,0 +1,74 @@
+import Footer from "../../components/footer";
+import Navbar from "../../components/navbar";
+
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <Navbar bgVariant="bg-white border-b border-b-secondary-0" />
+
+      <section className="mx-auto max-w-6xl xl:px-0 lg:px-10 md:px-10 px-3 mt-24 pb-5">
+        <h1 className="xl:text-7xl lg:text-6xl md:text-5xl text-2xl md:tracking-tighter font-bold font-oswald text-secondary-0">
+          Privacy Policy
+        </h1>
+
+        <div className="text-gray-400 text-base">
+          <p className="mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro rem
+            enim nesciunt non ullam sed doloremque repellendus voluptate ab
+            fugiat itaque deserunt nihil molestias error, ipsam consequuntur
+            velit sequi? Temporibus. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Porro rem enim nesciunt non ullam sed doloremque
+            repellendus voluptate ab fugiat itaque deserunt nihil molestias
+            error, ipsam consequuntur velit sequi? Temporibus. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Porro rem enim nesciunt non
+            ullam sed doloremque repellendus voluptate ab fugiat itaque deserunt
+            nihil molestias error, ipsam consequuntur velit sequi? Temporibus.
+          </p>
+
+          <p className="mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro rem
+            enim nesciunt non ullam sed doloremque repellendus voluptate ab
+            fugiat itaque deserunt nihil molestias error, ipsam consequuntur
+            velit sequi? Temporibus. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Porro rem enim nesciunt non ullam sed doloremque
+            repellendus voluptate ab fugiat itaque deserunt nihil molestias
+            error, ipsam consequuntur velit sequi? Temporibus. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Porro rem enim nesciunt non
+            ullam sed doloremque repellendus voluptate ab fugiat itaque deserunt
+            nihil molestias error, ipsam consequuntur velit sequi? Temporibus.
+          </p>
+
+          <p className="mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro rem
+            enim nesciunt non ullam sed doloremque repellendus voluptate ab
+            fugiat itaque deserunt nihil molestias error, ipsam consequuntur
+            velit sequi? Temporibus. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Porro rem enim nesciunt non ullam sed doloremque
+            repellendus voluptate ab fugiat itaque deserunt nihil molestias
+            error, ipsam consequuntur velit sequi? Temporibus. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Porro rem enim nesciunt non
+            ullam sed doloremque repellendus voluptate ab fugiat itaque deserunt
+            nihil molestias error, ipsam consequuntur velit sequi? Temporibus.
+          </p>
+
+          <p className="mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro rem
+            enim nesciunt non ullam sed doloremque repellendus voluptate ab
+            fugiat itaque deserunt nihil molestias error, ipsam consequuntur
+            velit sequi? Temporibus. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Porro rem enim nesciunt non ullam sed doloremque
+            repellendus voluptate ab fugiat itaque deserunt nihil molestias
+            error, ipsam consequuntur velit sequi? Temporibus. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Porro rem enim nesciunt non
+            ullam sed doloremque repellendus voluptate ab fugiat itaque deserunt
+            nihil molestias error, ipsam consequuntur velit sequi? Temporibus.
+          </p>
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
+};
+
+export default PrivacyPolicy;
